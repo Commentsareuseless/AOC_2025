ee39f91555c281035fccd7111a495c6fe2661845
Commentsareuseless <55753818+Commentsareuseless@users.noreply.github.com>
2025-12-12 13:20:00 +0100

    Solution for day3 part 2

--- day3/BatteryJoltageP2.cpp
+++ day3/BatteryJoltageP2.cpp
@@ -19,25 +19,26 @@ inline constexpr std::initializer_list<const char* const> VERBOSE_FLAG_ALTS{
     common::KnownParams::VERBOSE_FLAG, common::KnownParams::VERBOSE_FLAG_SHORT};
 inline constexpr std::initializer_list<const char* const> INPUT_FILES_ALTS{
     common::KnownParams::INPUT_FILE, common::KnownParams::INPUT_FILE_SHORT};
-inline constexpr size_t MAX_NUM_OF_TURNED_ON_BATTERIES{2};
+inline constexpr size_t MAX_NUM_OF_TURNED_ON_BATTERIES{12};
 
 template <size_t BankSize, typename Item_t = uint8_t>
 class UsefulBatteryBank
 {
 public:
   void append(const uint8_t newValue) {
-    if (shouldBeShiftedLeft()) { shiftLeft(); }
+    const uint32_t shiftSquashPos{shouldBeShiftedLeft()};
+    shiftLeft(shiftSquashPos);
 
     if (bank.back() < newValue) { bank.back() = newValue; }
   }
 
-  uint32_t joltage() const {
-    constexpr uint32_t RADIX{10};
+  size_t joltage() const {
+    constexpr size_t RADIX{10};
 
     if constexpr (BankSize == 2) {
       return bank[0] * RADIX + bank[1];
     } else {
-      uint32_t combinedJoltage{0};
+      size_t combinedJoltage{0};
       for (const auto battJoltage : bank) {
         combinedJoltage += battJoltage;
         combinedJoltage *= RADIX;
@@ -46,29 +47,45 @@ public:
     }
   }
 
-  void printFoundJoltage() {
-    lg::printInf("Highest joltage found: [{}, {}]", bank[0], bank[1]);
-  }
-
 private:
-  bool shouldBeShiftedLeft() const {
+  /**
+   * @brief Should the battery bank joltages be shifted
+   *        to make room for more (larger)batteries.
+   *
+   * @return 0 if shift is not required, position of digit
+   *         that will be "squashed" by shift if necessary
+   * Example: bank: [8 1 8 1 8 1 9]
+   * shouldBeShifted? -> Yes, because 8(idx: 2) > 1(idx: 1)
+   * shiftPos -> 1 -> digits from idx:6 to idx: 1 should be shifted left
+   * Shifting result -> [8 8 1 8 1 9 0]
+   * Shifting bank left will make a room for another digit, that will make
+   * a final number larger.
+   * 8818190 > 8181819
+   */
+  uint32_t shouldBeShiftedLeft() const {
     for (size_t idx{0}; idx < (BankSize - 1); ++idx) {
       if (bank.at(idx) < bank.at(idx + 1)) {
         /* Higher "radix" digit is lower than lower "radix" */
         /* so shifting this number left will produce higher number */
         /* e.g.: 59 -> *leftShift* -> 90 */
-        return true;
+        return idx;
       }
     }
 
-    return false;
+    return BankSize;
   }
 
   /* Could have used operator overload but I don't like them */
-  void shiftLeft() {
+  /**
+   * @brief Shift whole bank vector by one position
+   *
+   * @param squashPos - Position that will be "squashed" during shift
+   * Example: bank: [1 2 3 5 6] -> *shift, squashPos: 1* -> [1 3 5 6 0]
+   */
+  void shiftLeft(uint32_t squashPos) {
     Item_t prevValue{0};
-    for (auto itr{bank.rbegin()}; itr != bank.rend(); ++itr) {
-      Item_t& currentVal{*itr};
+    for (size_t idx{BankSize}; idx > squashPos; --idx) {
+      Item_t& currentVal{bank.at(idx - 1)};
       Item_t tmp{currentVal};
 
       currentVal = prevValue;
@@ -88,11 +105,10 @@ constexpr uint8_t charToDigit(char input) {
   return digit - FIRST_DIGIT_IN_ASCII;
 }
 
-uint32_t sumJoltageInBank(const std::string& inputBank) {
+size_t sumJoltageInBank(const std::string& inputBank) {
   UsefulBatteryBank<MAX_NUM_OF_TURNED_ON_BATTERIES> usefulBank{};
   for (const char bat : inputBank) { usefulBank.append(charToDigit(bat)); }
 
-  usefulBank.printFoundJoltage();
   lg::printInf("Combined Joltage: {}", usefulBank.joltage());
   return usefulBank.joltage();
 }
